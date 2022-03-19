ede8e3957cd945105632394f95e6b8e72b7ada11
Ela Jahoda <3la.jah0da@gmail.com>
2022-03-19 21:53:34 +0100

    bonus pro holky

--- vyvolavac.js
+++ vyvolavac.js
@@ -28,6 +28,8 @@ let jmena = [
 ];
 
 let tazenaJmena = [];
+let vyherce = document.querySelector('#vyherka');
+let pocet = document.querySelector('#pocitadlo');
 
 function tahnoutJmeno() {
     if (jmena.length === 0) {
@@ -43,12 +45,48 @@ function tahnoutJmeno() {
     // Vyřadíme vylosované jméno z osudí
     jmena.splice(vyherniIndex, 1);
 
-    // Výherní jméno si uložíme do pole k ostatním výherním
-    tazenaJmena.push(vyherniJmeno);
+    // Výherní jméno si uložíme do pole k ostatním výherním na zacatek
+    tazenaJmena.unshift(vyherniJmeno);
 
-    let vyherce = document.querySelector('#vyherka');
     vyherce.textContent = vyherniJmeno;
 
     let seznam = document.querySelector('#seznam');
     seznam.textContent = tazenaJmena;
+
+    pocitadlo.textContent = parseFloat(pocitadlo.textContent) + 1;
+}
+
+function restart() {
+    jmena = [
+        "Markéta",
+        "Kateřina",
+        "Júlia",
+        "Lucie",
+        "Aneta",
+        "Veronika",
+        "Petra",
+        "Olga",
+        "Naďa",
+        "Nikola",
+        "Eva",
+        "Anísa",
+        "Terezia",
+        "Diana",
+        "Anna",
+        "Iva",
+        "Denisa",
+        "Zuzana",
+        "Jana",
+        "Martina",
+        "Lenka",
+        "Iveta",
+        "Pavla",
+        "Lucie",
+        "Kamila",
+        "Soňa"
+    ];
+    tazenaJmena = [];
+    pocitadlo.textContent = 0;
+    seznam.textContent = tazenaJmena;
+    vyherce.textContent = ' ';
 }
